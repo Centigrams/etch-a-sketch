57f7725bff000683bf00a0f468d1ae293572770d
Sidd Bairan <bairansa@gmail.com>
2021-07-03 18:24:21 +0800

    Add customize grid size feature with prompt.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const container = document.querySelector('.container');
-const reset = document.querySelector('#reset-button');
+const reset = document.querySelector('#reset-button')
+let defaultGridSize = 16;
 
 function createGrid(gridSize) {
+    // Create grid with inserted divs.
     let gridArea = gridSize * gridSize;
     for (let i = 1; i <= gridArea; i++) {
-        let gridItem = document.createElement('div');
+        let gridBlock = document.createElement('div');
         container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
         container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-        container.insertAdjacentElement('beforeend', gridItem);
+        container.insertAdjacentElement('beforeend', gridBlock);
     }
+    // After creating grid, add event listeners per cell (div).
     let gridCells = document.querySelectorAll('div');
     gridCells.forEach(gridCell => gridCell.addEventListener('mouseover', colorGrid));
 }
@@ -17,10 +20,20 @@ function colorGrid() {
     this.style.backgroundColor = 'black';
 }
 
-function resetGridColor() {
+function resetGrid() {
+    let userInput = parseInt(prompt("Enter grid size. (2-100)"))
     let gridCells = document.querySelectorAll('div');
-    gridCells.forEach(gridCell => gridCell.style.backgroundColor = "white")
+    gridCells.forEach(gridCell => gridCell.style.backgroundColor = "white");
+
+    if (userInput >= 100) {
+        createGrid(100);
+    } else if (userInput <= 2) {
+        createGrid(2);
+    } else {
+        createGrid(userInput);
+    }
 }
 
-createGrid(50);
-reset.addEventListener('click', resetGridColor);
+createGrid(defaultGridSize);
+
+reset.addEventListener('click', resetGrid);
